217700b0b2faf44a0000ea37a77241b7dbf0ab74
Philippe Haerens <haerphi@gmail.com>
2019-08-29 15:52:36 +0200

    final + readme.md

---
+++ index.js
@@ -0,0 +1,33 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+
+function convert(path) {
+    var text = fs.readFileSync(path, 'utf8');
+    console.log(text);
+    let newOne = ``;
+    let escaped = `\`\\`;
+    for (let i = 0; i < text.length; i++) {
+        for (let h = 0; h < escaped.length; h++) {
+            if (text[i] === escaped[h]) {
+                newOne += `\\`;
+                break;
+            }
+        }
+        newOne += text[i];
+    }
+
+    fs.writeFile(path, newOne, function (err) {
+        if (err) throw err;
+        console.log('Saved!');
+    });
+}
+
+let path = process.argv[2];
+if (path != undefined) {
+    convert(path);
+} else {
+    console.log('Merci de rajouter un argument');
+}
+
+module.exports = convert;
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Text art escape carater
+Hi, welcome in my npm package to convert your text art !
+## Warning
+The convert edit the file, I doesn't create a new file with the result.
+## Setup
+### Git
+ - Clone the repertory : `git clone https://github.com/haerphi/NodeEscapeCharacter.git`
+ - Move in the folder
+ - Install depedencies : `npm i` or `npm install`
+ - Convert your file : `node . PathToYourFile``
+ - Enjoy
+### Npm
+Coming soon
